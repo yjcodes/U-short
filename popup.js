de63e004057b64abe9ea86046735b435e0a17665
Yuvansh <yuvansh101@gmail.com>
2021-02-08 23:36:02 +0530

    v1.0

---
+++ popup.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+	chrome.windows.getCurrent(function (currentWindow) {
+		chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
+			var url = activeTabs[0].url;
+			var str = "url=" + url;
+
+			var xhr = new XMLHttpRequest();
+			xhr.open("PUT", "https://ur2.pl/api/addLink", true);
+			xhr.onreadystatechange = function () {
+				if(xhr.readyState == 4) {
+					var response = JSON.parse(xhr.responseText);
+					var responseContainer = document.getElementById('response');
+
+					if(!response.error && !response.zonk) {
+						responseContainer.innerHTML = response.fullURL;
+					}
+					else {
+						responseContainer.innerHTML = response.msg;
+					}
+				}
+			};
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhr.send(str);
+
+			$(function(){
+				$('#sendurl').click(function(){
+					chrome.tabs.create({ url: url });
+				});
+			  });
+
+			  new ClipboardJS('.btn');
+
+		});
+	});
+}; 
